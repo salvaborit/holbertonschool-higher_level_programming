e1ee37c726a0828857d46892b03463675e314c6f
Salvador Borit <5084@holbertonschool.com>
2022-09-06 14:15:11 -0300

    attempt 1

--- python-if_else_loops_functions/9-print_last_digit.py
+++ python-if_else_loops_functions/9-print_last_digit.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 
+
+def print_last_digit(number):
+    number_u = number * -1
+    if number < 0:
+        print(f"{number_u % 10}", end="")
+        return number_u % 10
+    elif number > 0:
+        print(f"{number % 10}", end="")
+        return number % 10
+    else:
+        print(f"{number}", end="")
+        return number
