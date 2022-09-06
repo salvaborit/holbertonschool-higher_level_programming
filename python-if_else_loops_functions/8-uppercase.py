a736407de7dff81ba1d60a256ce31df16e8e275e
Salvador Borit <5084@holbertonschool.com>
2022-09-06 13:45:09 -0300

    fix case for empty str

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -3,9 +3,9 @@
 
 
 def uppercase(str):
-    x = len(str)
     for i in str:
         if i >= 'a' and i <= 'z':
             i = chr(ord(i) - 32)
-        print("{:s}".format(i), end=("\n" if x < 2 else ""))
-        x -= 1
+        print("{:s}".format(i), end="")
+    else:
+        print()
