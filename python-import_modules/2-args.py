9f94c6a2bd6ee5f89358655a1c21b2f3daad572e
Salvador Borit <5084@holbertonschool.com>
2022-09-07 19:44:07 -0300

    attempt 1

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,2 +1,18 @@
 #!/usr/bin/python3
 
+
+from platform import python_branch
+
+
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) == 1:
+        print("0 arguments.")
+    elif len(sys.argv) == 2:
+        print("1 argument:")
+    elif len(sys.argv) > 2:
+        print(f"{len(sys.argv) - 1} arguments:")
+    x = 1
+    for arg in sys.argv[1:]:
+        print(f"{x}: {arg}")
+        x += 1
