a68d4e4be7ea181a81a62305452e369419e72395
Salvador Borit <5084@holbertonschool.com>
2022-09-07 22:20:30 -0300

    first attempt

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 
-from platform import python_branch
-
-
 if __name__ == "__main__":
     import sys
     if len(sys.argv) == 1:
--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 
+
+if __name__ == "__main__":
+    import sys
+    sum = 0
+    for addend in sys.argv[1:]:
+        sum += int(addend)
+    print(sum)
