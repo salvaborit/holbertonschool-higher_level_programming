8fffe7af707a598996ad24efbce64a1da6cd7dfb
Salvador Borit <5084@holbertonschool.com>
2022-09-12 20:15:20 -0300

    pep8

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -5,4 +5,4 @@ def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         return
     for int in my_list[::-1]:
-      print("{:d}".format(int))
+        print("{:d}".format(int))
