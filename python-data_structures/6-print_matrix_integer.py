b09d45c0ec146d1396d741d22f53bd39b9684a8d
Salvador Borit <5084@holbertonschool.com>
2022-09-12 21:36:46 -0300

    fix for every matrix size

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -2,8 +2,8 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    for full_row in matrix:
+    for row in matrix:
         idx = 0
-        for i in full_row:
-            print("{:d}".format(i), end=" " if idx < 2 else "\n")
+        for i in row:
+            print("{:d}".format(i), end=" " if idx < len(row) - 1 else "\n")
             idx += 1
