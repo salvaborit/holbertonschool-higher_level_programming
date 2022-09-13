6a5edf6218d1414d06df9c65e3faa3de0cc7da69
Salvador Borit <5084@holbertonschool.com>
2022-09-13 20:41:46 -0300

    fix

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -2,4 +2,8 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    tuple_a = tuple_a + ((0,) * (len(tuple_b) - len(tuple_a)))
+    tuple_b = tuple_b + ((0,) * (len(tuple_a) - len(tuple_b)))
+    zipped = zip(tuple_a[:2], tuple_b[:2])
+    mapped = map(sum, zipped)
+    return tuple(mapped)
