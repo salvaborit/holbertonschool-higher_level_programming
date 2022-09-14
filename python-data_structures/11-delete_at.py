6d97b6406db03205434b75987b03cc208c9a205c
Salvador Borit <5084@holbertonschool.com>
2022-09-14 00:50:41 -0300

    attempt 1

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 
-def delete_at(my_list=[], idx=0):
+from dataclasses import asdict
 
+
+def delete_at(my_list=[], idx=0):
+    if idx in range(0, len(my_list)):
+        del(my_list[idx])
+    return my_list
