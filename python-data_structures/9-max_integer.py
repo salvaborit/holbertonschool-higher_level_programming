e2f234db04d57f69ef38fb0bcaedf4be24f017fe
Salvador Borit <5084@holbertonschool.com>
2022-09-13 22:18:19 -0300

    attempt 1

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+from queue import Empty
+
+
+def max_integer(my_list=[]):
+    max = 0
+    if list is not Empty:
+        for i in my_list:
+            if i > max:
+                max = i
+        return max
+    return None
