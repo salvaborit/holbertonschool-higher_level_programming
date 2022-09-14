5ec866c0f168ea4d2d7a3bf23784a065d48b244e
Salvador Borit <5084@holbertonschool.com>
2022-09-13 22:13:16 -0300

    optimized

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -2,6 +2,4 @@
 
 
 def multiple_returns(sentence):
-    if sentence != "":
-        return (len(sentence), sentence[0])
-    return (0, None)
+    return (len(sentence), sentence[0]) if sentence != "" else (0, None)
