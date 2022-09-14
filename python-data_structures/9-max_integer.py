b5778c6c855969e4410c441c32d1ccdffbeb0ed7
Salvador Borit <5084@holbertonschool.com>
2022-09-13 22:35:34 -0300

    fix for empty list

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -2,10 +2,10 @@
 
 
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
     max = 0
-    if list is not []:
-        for i in my_list:
-            if i > max:
-                max = i
-        return max
-    return None
+    for i in my_list:
+        if i > max:
+            max = i
+    return max
