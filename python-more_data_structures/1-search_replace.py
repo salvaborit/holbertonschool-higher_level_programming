af92d787502e18307dc48b8194be148033c2f1eb
Salvador Borit <5084@holbertonschool.com>
2022-09-16 17:55:29 -0300

    attempt 1

---
+++ python-more_data_structures/1-search_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+# @my_list: is the initial list
+# @search: is the element to replace in the list
+# @replace: is the new element
+
+def search_replace(my_list, search, replace):
+    new_list = []
+    for elem in my_list:
+        if elem is search:
+            new_list.append(replace)
+        else:
+            new_list.append(elem)
+    return new_list
