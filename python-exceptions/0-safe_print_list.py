d12a66c26313a6ccc06481dc76aa983dd885a548
Salvador Borit <5084@holbertonschool.com>
2022-09-17 20:45:34 -0300

    attempt 1

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
 
-def safe_print_list(my_list=[], x=0):
+from logging import exception
 
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        numbers_printed = 0
+        for item in my_list:
+            if numbers_printed == x:
+                break
+            print(item, end='')
+            numbers_printed += 1
+        print()
+        return numbers_printed
+    except Exception:
+        return numbers_printed
