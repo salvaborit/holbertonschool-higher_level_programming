2bc217cf2e912c17d33b9672ba2b4f9cef2cb5a9
Salvador Borit <5084@holbertonschool.com>
2022-09-17 13:29:36 -0300

    attempt 1

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
 
-from unittest.mock import NonCallableMagicMock
-
-
 def best_score(a_dictionary):
     if a_dictionary is not None:
         return max(a_dictionary, key=a_dictionary.get)
