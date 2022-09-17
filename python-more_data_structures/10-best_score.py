7ab7afe823139917f988635ca238b606ad603e73
Salvador Borit <5084@holbertonschool.com>
2022-09-17 13:30:48 -0300

    fix for empty dict

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -2,7 +2,7 @@
 
 
 def best_score(a_dictionary):
-    if a_dictionary is not None:
-        return max(a_dictionary, key=a_dictionary.get)
-    else:
+    if a_dictionary is None or a_dictionary == {}:
         return None
+    else:
+        return max(a_dictionary, key=a_dictionary.get)
