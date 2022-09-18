3662691155e0198565481eee1102f5759fbd4f46
Salvador Borit <5084@holbertonschool.com>
2022-09-18 16:04:21 -0300

    attempt 1

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -2,10 +2,38 @@
 
 
 def roman_to_int(roman_string):
-    if roman_string is None or str(isinstance(roman_string, str)) is True:
+    if not roman_string or type(roman_string) is not str:
         return None
-    roman_dict = {'I': 1, 'V': 5, 'X': 10,
-        'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
-    for numeral in roman_string:
-        if numeral
+    main = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    exc = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
+    sum = 0
+    # numerals = list(map(lambda i: roman_dict[i], roman_string))
+
+    # find idx of exceptions and save in list like: (index of exc, 'exception')
+    exc_keys = exc.keys()
+    exc_indexes = []
+    start = 0
+    while start < len(roman_string) and start != -1:
+        for key in exc_keys:
+            exc_index = roman_string.find(key)
+            if exc_index != -1:
+                exc_indexes.append((exc_index, key))
+                start = exc_index + 1
+            else:
+                start += 1
+
+    # start adding while taking exceptions into account
+    roman_idx = 0
+    exc_found = False
+    while roman_idx < len(roman_string):
+        for exc_idx in exc_indexes:
+            if roman_idx == exc_idx[0]:
+                sum += exc[exc_idx[1]]
+                exc_found = True
+                break
+        if exc_found is False:
+            sum += main[roman_string[roman_idx]]
+        roman_idx += 1
+
+    return sum
