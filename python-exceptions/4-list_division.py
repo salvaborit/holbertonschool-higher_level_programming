9e32d6e980d842e5f4ff48f3ece2256fbb55a035
Salvador Borit <5084@holbertonschool.com>
2022-09-18 14:27:08 -0300

    attempt 1

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -2,4 +2,20 @@
 
 
 def list_division(my_list_1, my_list_2, list_length):
-
+    new_list = []
+    for i in range(0, list_length):
+        try:
+            quotient = my_list_1[i] / my_list_2[i]
+            new_list.append(quotient)
+        except ZeroDivisionError:
+            new_list.append(0)
+            print("division by zero")
+        except TypeError:
+            new_list.append(0)
+            print("wrong type")
+        except IndexError:
+            new_list.append(0)
+            print("out of range")
+        finally:
+            pass
+    return new_list
