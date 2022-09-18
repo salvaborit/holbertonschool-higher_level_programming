b9d13188cc286adbdbd06b18230fadcaf56efbb3
Salvador Borit <5084@holbertonschool.com>
2022-09-18 14:07:49 -0300

    attempt 1

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -2,4 +2,10 @@
 
 
 def safe_print_division(a, b):
-
+    try:
+        quotient = a / b
+    except ZeroDivisionError:
+        quotient = None
+    finally:
+        print("Inside result: {}".format(quotient))
+        return quotient
