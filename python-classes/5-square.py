75773ee0d9bca5fb87de559111495f382379810a
Salvador Borit <5084@holbertonschool.com>
2022-09-19 21:47:50 -0300

    attempt1

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -2,4 +2,44 @@
 """ Python interpreter """
 
 
+class Square:
+    """On instantiation
+    Args:
+        size (int): size of square
+    """
 
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    @property
+    def size(self):
+        """Gets size of instance"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets size of instance
+        Args:
+            value (int): value to assign to __size of instance
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = value
+
+    def area(self):
+        """Returns area of instance (square)"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints the square with # (hashbangs)"""
+        for i in range(0, self.__size):
+            for i in range(0, self.__size):
+                print(f"#", end='' if i < self.__size - 1 else '\n')
