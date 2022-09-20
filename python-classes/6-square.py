ce41e5e1f8fed30079c090fbe7da02f7894f34a2
Salvador Borit <5084@holbertonschool.com>
2022-09-19 22:54:48 -0300

    fix for string in typle

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -13,7 +13,7 @@ class Square:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
-        if type(position[0]) != int or type(position[1]) != int:
+        if type(position[0]) != int or len(position) != 2 or type(position[1]) != int:
             raise TypeError('position must be a tuple of 2 positive integers')
         if position[0] < 0 or position[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
