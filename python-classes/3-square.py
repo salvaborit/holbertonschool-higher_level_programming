a1cb1212d281b444c7461d3784b4203d3039275d
Salvador Borit <5084@holbertonschool.com>
2022-09-19 21:43:50 -0300

    bug

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -7,9 +7,9 @@ class Square:
     def __init__(self, size=0):
 
         if type(size) is not int:
-            raise TypeError('size must be >= 0')
+            raise TypeError('size must be an integer')
         if size < 0:
-            raise(ValueError('size must be >= 0'))
+            raise ValueError('size must be >= 0')
 
         self.__size = size
 
