3302182a7fe3a01de213e36f35d26efa07e8de57
Salvador Borit <5084@holbertonschool.com>
2022-09-19 21:42:30 -0300

    fix error messages

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -10,7 +10,7 @@ class Square:
 
     def __init__(self, size=0):
         if type(size) is not int:
-            raise TypeError('size must be >= 0')
+            raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
 
@@ -28,7 +28,7 @@ class Square:
             value (int): value to assign to __size of instance
         """
         if type(value) is not int:
-            raise TypeError('size must be >= 0')
+            raise TypeError('size must be an integer')
         if value < 0:
             raise ValueError('size must be >= 0')
 
