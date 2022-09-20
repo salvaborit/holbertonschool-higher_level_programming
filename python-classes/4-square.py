8934469f05cc96d1e3cd2cc5b56e860dc7a796f7
Salvador Borit <5084@holbertonschool.com>
2022-09-19 21:33:07 -0300

    test

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -3,10 +3,35 @@
 
 
 class Square:
-    """ class: Square """
 
-    def __init__(self, size):
+    def __init__(self, size=0):
+        """On instantiation
+        Args:
+            size (int): size of square
+        """
         if type(size) is not int:
             raise TypeError('size must be >= 0')
         if size < 0:
-            raise(ValueError('size must be >= 0'))
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+
+    def size(self):
+        """Gets size of instance"""
+        return self.__size
+
+    def size(self, value):
+        """Sets size of instance
+        Args:
+            value (int): value to assign to __size of instance
+        """
+        if type(value) is not int:
+            raise TypeError('size must be >= 0')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = value
+
+    def area(self):
+        """Returns area of instance (square)"""
+        return self.__size * self.__size
