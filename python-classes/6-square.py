34d51fa328f667f203828c893a5c2cb38132c3e7
Salvador Borit <5084@holbertonschool.com>
2022-09-19 22:44:43 -0300

    fix init position set

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -13,6 +13,8 @@ class Square:
             raise TypeError('size must be an integer')
         if size < 0:
             raise ValueError('size must be >= 0')
+        if type(position) is not tuple or position[0] < 0 or position[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
 
         self.__size = size
         self.__position = position
