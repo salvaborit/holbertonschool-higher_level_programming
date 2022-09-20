d5ff449389bc5ef6a15a59f93fd59d955c2d8d40
Salvador Borit <5084@holbertonschool.com>
2022-09-20 18:30:09 -0300

    tests

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -4,7 +4,7 @@
 
 def add_integer(a, b=98):
     """Adds two ints"""
-    if type(a) is not int and type(a) is not float:
+    if a is None or type(a) is not int and type(a) is not float:
         raise TypeError('a must be an integer')
     if type(b) is not int and type(b) is not float:
         raise TypeError('b must be an integer')
--- python-test_driven_development/0-main.py
+++ python-test_driven_development/0-main.py
@@ -13,3 +13,5 @@ try:
     print(add_integer(None))
 except Exception as e:
     print(e)
+
+print(f"float overflow = {add_integer(float('inf'))}")
