ef241ee72996cd2cd14238f751acd6001941a079
Salvador Borit <5084@holbertonschool.com>
2022-09-19 21:06:58 -0300

    attempt1

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,4 +2,16 @@
 """ Python interpreter """
 
 
+class Square:
+    """ class: Square """
+    def __init__(self, size=0):
 
+        if type(size) is not int:
+            raise TypeError('size must be >= 0')
+        if size < 0:
+            raise(ValueError('size must be >= 0'))
+
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -2,4 +2,11 @@
 """ Python interpreter """
 
 
+class Square:
+    """ class: Square """
 
+    def __init__(self, size):
+        if type(size) is not int:
+            raise TypeError('size must be >= 0')
+        if size < 0:
+            raise(ValueError('size must be >= 0'))
