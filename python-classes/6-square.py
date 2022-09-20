875ca5c89e71cc220895ce3ca047e2ffa4be51b3
Salvador Borit <5084@holbertonschool.com>
2022-09-19 22:42:35 -0300

    attempt1

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -2,4 +2,72 @@
 """ Python interpreter """
 
 
+class Square:
+    """On instantiation
+    Args:
+        size (int): size of square
+    """
 
+    def __init__(self, size=0, position=(0, 0)):
+        if type(size) is not int:
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = size
+        self.__position = position
+
+    @property
+    def size(self):
+        """Gets size of instance"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Sets size of instance
+        Args:
+            value (int): value to assign to __size of instance
+        """
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        if value < 0:
+            raise ValueError('size must be >= 0')
+
+        self.__size = value
+
+    @property
+    def position(self):
+        """Gets size of position"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """Sets size of position
+        Args:
+            value (tuple): a tuple of 2 positive ints
+        """
+        if type(value) is not tuple or value[0] < 0 or value[1] < 0:
+            raise TypeError('position must be a tuple of 2 positive integers')
+
+        self.__position = value
+
+    def area(self):
+        """Returns area of instance (square)"""
+        return self.__size * self.__size
+
+    def my_print(self):
+        """Prints the square with # (hashbangs)"""
+        if self.__size == 0:
+            print()
+            return
+
+        if self.__position[1] > 0:
+            for i in range(self.__position[1]):
+                print()
+
+        for row in range(self.__size):
+            if self.__position[0] > 0:
+                for i in range(self.__position[0]):
+                    print(' ', end='')
+            for cols in range(self.__size):
+                print("#", end='' if cols < self.__size - 1 else '\n')
