a3ccdbb2865dbb9406c9a5045a80b120984c4399
Salvador Borit <5084@holbertonschool.com>
2022-09-21 01:04:41 -0300

    unittest test

--- python-test_driven_development/tests/6-max_integer_test.py
+++ python-test_driven_development/tests/6-max_integer_test.py
@@ -2,4 +2,14 @@
 """Python interpreter"""
 
 
+max_integer = __import__('6-max_integer').max_integer
+import unittest
 
+class TestMaxInteger(unittest.TestCase):
+    """unittest for max integer"""
+
+    def test_max_end(self):
+        self.assertTrue(max_integer([1, 2, 3]), 3)
+
+if __name__ == '__main__':
+    unittest.main()
