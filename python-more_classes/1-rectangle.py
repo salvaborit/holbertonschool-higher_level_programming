e25bda90c3f00e5abd5e16d730fb2e21c42b2acc
Salvador Borit <5084@holbertonschool.com>
2022-09-21 17:38:39 -0300

    attempt1

--- python-more_classes/1-rectangle.py
+++ python-more_classes/1-rectangle.py
@@ -2,4 +2,50 @@
 """Python interpreter"""
 
 
+class Rectangle:
+    """class: Rectangle"""
 
+    def __init__(self, width=0, height=0):
+        """On instantiation"""
+
+        if type(width) is not int:
+            raise TypeError('width must be an integer')
+        if width < 0:
+            raise ValueError('width must be >= 0')
+        if type(height) is not int:
+            raise TypeError('height must be an integer')
+        if height < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Width setter"""
+        if type(value) is not int:
+            raise TypeError('width must be an integer')
+        if value < 0:
+            raise ValueError('width must be >= 0')
+
+        self.__width = value
+
+    @property
+    def height(self):
+        """Height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Height setter"""
+        if type(value) is not int:
+            raise TypeError('height must be an integer')
+        if value < 0:
+            raise ValueError('height must be >= 0')
+
+        self.__height = value
