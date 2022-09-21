d7d8f65ee6bf22a9400f3c2d3c1e8f282b4941ee
Salvador Borit <5084@holbertonschool.com>
2022-09-20 22:48:25 -0300

    attempt1

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -2,4 +2,13 @@
 """Python interpreter"""
 
 
+def text_indentation(text):
+    # delims = [".", "?", ":"]
+    # for i in delims:
+    #     text.replace(i, f"{i}\n")
+    new_text = ""
+    new_text += text.replace(".", ".\n\n")
+    new_text += text.replace("?", "?\n\n")
+    new_text += text.replace(":", ":\n\n")
 
+    print(new_text)
