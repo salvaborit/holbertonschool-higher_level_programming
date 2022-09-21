a1a393cd2d0bc8e93394481bfb24a3dfef05f784
Salvador Borit <5084@holbertonschool.com>
2022-09-20 22:00:01 -0300

    fix error message

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -6,14 +6,12 @@ def print_square(size):
     """Prints a square of hashbangs (#)"""
 
     if type(size) is not int:
-        raise TypeError('size must be an int')
+        raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
     if type(size) is float and size < 0:
         raise TypeError('size must be an integer')
 
     for row in range(size):
-        # col_count = 0
         for col in range(size):
             print("#", end='' if col != size - 1 else '\n')
-            # col_count += 1
