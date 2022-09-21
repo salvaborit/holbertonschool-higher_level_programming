d2cfe67cb14b4df702fccf3be419f4fb6e1a2735
Salvador Borit <5084@holbertonschool.com>
2022-09-20 22:53:21 -0300

    error handling and doc

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -3,9 +3,13 @@
 
 
 def text_indentation(text):
-    # delims = [".", "?", ":"]
-    # for i in delims:
-    #     text.replace(i, f"{i}\n")
+    """Prints a text with 2 new lines after each of these characters:
+        ".", "?" and ":"
+     """
+
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+
     new_text = ""
     new_text += text.replace(".", ".\n\n")
     new_text += text.replace("?", "?\n\n")
