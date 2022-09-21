7e6b01fe9e32c796c679239157c4261095f10720
Salvador Borit <5084@holbertonschool.com>
2022-09-21 00:10:52 -0300

    re syntaxed

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -10,14 +10,18 @@ def text_indentation(text):
     if type(text) is not str:
         raise TypeError('text must be a string')
 
-    new_text = ""
-    new_text = text.replace(". ", ".\n")
-    new_text = new_text.replace(".", ".\n")
-    new_text = new_text.replace("? ", "?\n")
-    new_text = new_text.replace("?", "?\n")
-    new_text = new_text.replace(": ", ":\n")
-    new_text = new_text.replace(":", ":\n")
+    # new_text = ""
+    # new_text = text.replace(". ", ".\n")
+    # new_text = new_text.replace(".", ".\n")
+    # new_text = new_text.replace("? ", "?\n")
+    # new_text = new_text.replace("?", "?\n")
+    # new_text = new_text.replace(": ", ":\n")
+    # new_text = new_text.replace(":", ":\n")
 
-
-
-    print(new_text, end='')
+    start = 0
+    end = len(text)
+    for idx in range(len(text)):
+        if text[idx] in ".:?":
+            end = idx + 1
+            print(text[start:end].lstrip(), end='\n\n')
+            start = end
