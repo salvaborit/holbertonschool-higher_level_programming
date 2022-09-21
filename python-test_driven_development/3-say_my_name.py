fadd880ec7a242e947c327816719feb1f74673ba
Salvador Borit <5084@holbertonschool.com>
2022-09-20 21:44:08 -0300

    attempt1

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,4 +2,15 @@
 """Python interpreter"""
 
 
+from typing import Type
 
+
+def say_my_name(first_name, last_name=""):
+    """
+    """
+    if type(first_name) is not str:
+        raise TypeError('first_name must be a string')
+    if type(last_name) is not str:
+        raise TypeError('last_name must be a string')
+
+    print(f"my name is {first_name} {last_name}")
