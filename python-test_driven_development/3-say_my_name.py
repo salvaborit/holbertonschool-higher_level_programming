c3c3a37cd04a92a52a1bbf4cab85ad44b114292c
Salvador Borit <5084@holbertonschool.com>
2022-09-20 21:45:23 -0300

    doc and grammar

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -2,15 +2,11 @@
 """Python interpreter"""
 
 
-from typing import Type
-
-
 def say_my_name(first_name, last_name=""):
-    """
-    """
+    """Prints first and last name (params)"""
     if type(first_name) is not str:
         raise TypeError('first_name must be a string')
     if type(last_name) is not str:
         raise TypeError('last_name must be a string')
 
-    print(f"my name is {first_name} {last_name}")
+    print(f"My name is {first_name} {last_name}")
