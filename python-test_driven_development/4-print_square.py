3014819175ac2cde78a67391c26546ce69c72a7e
Salvador Borit <5084@holbertonschool.com>
2022-09-20 21:58:33 -0300

    attempt1

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -2,4 +2,18 @@
 """Python interpreter"""
 
 
+def print_square(size):
+    """Prints a square of # (hashbangs)"""
 
+    if type(size) is not int:
+        raise TypeError('size must be an int')
+    if size < 0:
+        raise ValueError('size must be >= 0')
+    if type(size) is float and size < 0:
+        raise TypeError('size must be an integer')
+
+    for row in range(size):
+        col_count = 0
+        for col in range(size):
+            print("#", end='' if col_count != size - 1 else '\n')
+            col_count+= 1
