4aeea22adf4e0b9ed9634101962561043edce7ce
Salvador Borit <5084@holbertonschool.com>
2022-09-21 21:45:17 -0300

    attempt1

--- python-more_classes/8-rectangle.py
+++ python-more_classes/8-rectangle.py
@@ -88,7 +88,4 @@ class Rectangle:
         if type(rect_2) is not Rectangle:
             raise TypeError('rect_2 must be an instance of Rectangle')
 
-        if rect_1.area() >= rect_2.area():
-            return rect_1
-        else:
-            return rect_2
+        return rect_1 if rect_1.area() >= rect_2.area() else rect_2
