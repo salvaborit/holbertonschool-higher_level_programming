665962e0b403c0ca7e7e030e94b248d655c03b11
Salvador Borit <5084@holbertonschool.com>
2022-09-26 17:41:06 -0300

    fix syntax

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -2,4 +2,9 @@
 """Python interpreter"""
 
 
-
+class MyList(list):
+    """"class: MyList (inherits from 'list')"""
+    def print_sorted(self):
+        new_list = self.copy()
+        new_list.sort()
+        print(new_list)
