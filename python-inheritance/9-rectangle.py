d08c8a604bcc8aacbe376c38a1011b28e9359dc1
Salvador Borit <5084@holbertonschool.com>
2022-09-26 22:13:58 -0300

    atempt1

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -2,4 +2,20 @@
 """Python interpreter"""
 
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
+class Rectangle(BaseGeometry):
+    """class: Rectangle (inherits from 'BaseGeometry')"""
+    def __init__(self, width, height):
+        """Constructor"""
+        super().integer_validator("width", width)
+        super().integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
