397e4a077ba3870fb0c8364140be6fab9476400d
Salvador Borit <5084@holbertonschool.com>
2022-09-27 19:21:08 -0300

    attempt1

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -2,4 +2,11 @@
 """Python interpreter"""
 
 
+import json
+"""JSON package"""
 
+
+def save_to_json_file(my_obj, filename):
+    """Serializes an object to a text file"""
+    with open(filename, 'a') as file:
+        json.dump(my_obj, file)
