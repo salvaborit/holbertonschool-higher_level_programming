2195b0d3308eb7c9ac31ed25a7351bcec1ea5a76
Salvador Borit <5084@holbertonschool.com>
2022-09-27 20:12:12 -0300

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 """Python interpreter"""
 
+import sys
+"""sys module"""
+import json
+"""JSON package"""
 
 
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+save_to_json_file(sys.argv[1:], "add_item.json")
