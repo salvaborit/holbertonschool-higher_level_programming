364d5420003596c4d71b32583a39e89b274c1295
Salvador Borit <5084@holbertonschool.com>
2022-09-27 14:30:21 -0300

    fix error msg

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -8,6 +8,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """class: Square (inherits from 'Rectangle')"""
     def __init__(self, size):
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -9,7 +9,7 @@ class Rectangle(BaseGeometry):
     """class: Rectangle (inherits from 'BaseGeometry')"""
     def __init__(self, width, height):
         """Constructor"""
-        super().integer_validator("width", width)
-        super().integer_validator("height", height)
+        self.integer_validator("width", width)
         self.__width = width
+        self.integer_validator("height", height)
         self.__height = height
