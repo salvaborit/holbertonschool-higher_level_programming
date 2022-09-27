cbb03750e1c502ae4fac4b647bfd0a0062392a6d
Salvador Borit <5084@holbertonschool.com>
2022-09-27 19:13:57 -0300

    attempt1

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
 """Python interpreter"""
 
+import json
+"""JSON module"""
 
 
+def from_json_string(my_str):
+    """Returns an Python object represented by a JSON string"""
+    return json.loads(my_str)
