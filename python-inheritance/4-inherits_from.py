dc96200a1644df746808d58717feff47ea397daf
Salvador Borit <5084@holbertonschool.com>
2022-09-26 21:08:22 -0300

    test

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -2,4 +2,9 @@
 """Python interpreter"""
 
 
-
+def inherits_from(obj, a_class):
+    """Checks if an object is an instance of a class that inherited
+    (directly or indirectly) from the specified class"""
+    if issubclass(type(obj), a_class):
+        return True
+    return False
