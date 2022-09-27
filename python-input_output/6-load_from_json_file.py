432ab5966288b81617c17f21351475811645cb8f
Salvador Borit <5084@holbertonschool.com>
2022-09-27 19:31:02 -0300

    attempt1

--- python-input_output/6-load_from_json_file.py
+++ python-input_output/6-load_from_json_file.py
@@ -2,4 +2,11 @@
 """Python interpreter"""
 
 
+import json
+"""JSON package"""
 
+def load_from_json_file(filename):
+    """Creates an object from a JSON file"""
+    with open(filename) as json_file:
+        new_obj = json.load(json_file)
+    return new_obj
