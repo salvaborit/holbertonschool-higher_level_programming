fe07d6df0efacc91f7c529fc5bad370821d7ca36
Salvador Borit <5084@holbertonschool.com>
2022-09-26 22:39:29 -0300

    attempt1

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -2,4 +2,17 @@
 """Python interpreter"""
 
 
+Rectangle = __import__('9-rectangle').Rectangle
 
+
+class Square(Rectangle):
+    """class: Square (inherits from 'Rectangle')"""
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+    def __str__(self):
+        return f'[Square] {self.__size}/{self.__size}'
