6d0c6d5a8484682f060ba5c432ff7d1d22f40cd0
Salvador Borit <5084@holbertonschool.com>
2022-09-27 16:05:01 -0300

    attempt1

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -2,4 +2,8 @@
 """Python interpreter"""
 
 
-
+def write_file(filename="", text=""):
+    """Writes a str to a text file and returns characters written"""
+    with open(filename, "w+") as file:
+        file.write(text)
+    return len(text)
