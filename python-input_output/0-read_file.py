580882133b90da32ff5fd34f32e5e1728bccf62e
Salvador Borit <5084@holbertonschool.com>
2022-09-27 15:51:53 -0300

    remove final newline

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """Reads file and prints to stdout"""
     with open(filename, encoding="utf-8") as file:
         buffer = file.read()
-    print(buffer)
+    print(buffer, end='')
