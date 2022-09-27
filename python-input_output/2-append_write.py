99f07aee250cebe8e2f137906c4a7295678d0921
Salvador Borit <5084@holbertonschool.com>
2022-09-27 18:14:34 -0300

    test

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -2,4 +2,9 @@
 """Python interpreter"""
 
 
-
+def append_write(filename="", text=""):
+    """Appends a string to a text file
+    Returns: number of characters added"""
+    with open(filename, "a") as file:
+        write_len = file.write(text)
+    return write_len
