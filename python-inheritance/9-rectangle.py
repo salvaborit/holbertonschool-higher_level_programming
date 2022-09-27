caba7d6c104dd44b6ac82d7735b1c07eb598dcf6
Salvador Borit <5084@holbertonschool.com>
2022-09-26 22:35:04 -0300

    fix9

--- python-inheritance/9-rectangle.py
+++ python-inheritance/9-rectangle.py
@@ -18,4 +18,4 @@ class Rectangle(BaseGeometry):
         return self.__width * self.__height
 
     def __str__(self):
-        return "[Rectangle] " + str(self.__width) + "/" + str(self.__height)
+        return f'[Rectangle] {self.__width}/{self.__height}'
