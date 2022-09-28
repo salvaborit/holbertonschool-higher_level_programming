608246f2af81c199353f9cc55b787e5eaa7bb8f7
Salvador Borit <5084@holbertonschool.com>
2022-09-27 21:01:09 -0300

    test

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -3,7 +3,7 @@
 
 
 
-import os.path, sys, json
+import sys, json
 """sys module"""
 
 
@@ -12,11 +12,10 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 if __name__ == '__main__':
     json_list = []
-    if os.path.exists('add_item.json'):
-        with open('add_item.json') as file:
-            json_list = load_from_json_file('add_item.json')
-
+    try:
+        json_list = load_from_json_file('add_item.json')
+    except Exception:
+        pass
     for i in range(1, len(sys.argv)):
         json_list.append(sys.argv[i])
-
     save_to_json_file(json_list, 'add_item.json')
