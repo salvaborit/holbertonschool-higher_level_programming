79806ee65a1fe9d79c4b34017c064cee39c14ea8
Salvador Borit <5084@holbertonschool.com>
2022-09-30 15:19:26 -0300

    test

---
+++ python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,61 @@
+#!/usr/bin/python3
+"""Python interpreter"""
+
+
+Base = __import__('base').Base
+
+class Rectangle(Base):
+    """class: Rectangle (inherits from 'Base')"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Constructor"""
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
+
+    """WIDTH s/g"""
+    @property
+    def width(self):
+        """Getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter"""
+        self.__width = value
+
+    """HEIGHT s/g"""
+    @property
+    def height(self):
+        """Getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter"""
+        self.__height = value
+
+
+    """X s/g"""
+    @property
+    def x(self):
+        """Getter"""
+        return self.x
+
+    @height.setter
+    def x(self, value):
+        """Setter"""
+        self.x = value
+
+    """X s/g"""
+    @property
+    def y(self):
+        """Getter"""
+        return self.y
+
+    @height.setter
+    def y(self, value):
+        """Setter"""
+        self.y = value
