4f5cab33f7edf98bac047f534319283394c60cf5
Salvador Borit <5084@holbertonschool.com>
2022-09-30 14:49:25 -0300

    test

---
+++ python-almost_a_circle/models/base.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Python interpreter"""
+
+
+class Base:
+    """class: Base"""
+
+
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Constructor"""
+        if id is not None:
+            self.id = id
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
