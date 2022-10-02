f8af8b2ca0d159e443c17ab534296b6f8d664b8c
Salvador Borit <5084@holbertonschool.com>
2022-10-02 19:40:49 -0300

    save to json file

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -33,11 +33,15 @@ class Base:
     def save_to_file(cls, list_objs):
         """Serialize JSON to file"""
         filename = f'{cls.__name__}.json'
-        json_list = []
         if list_objs is None:
             with open(filename, 'w') as file:
-                file.write(json.dumps(json_list))
-        for instance in list_objs:
-            json_list.append(cls.to_json_string(instance.to_dictionary()))
+                file.write('[]')
+            return
+        string = '['
+        for num, instance in enumerate(list_objs):
+            if num != 0:
+                string += ", "
+            string += cls.to_json_string(instance.to_dictionary())
+        string += ']'
         with open(filename, 'w') as file:
-                file.write(str(json_list))
+                file.write(string)
