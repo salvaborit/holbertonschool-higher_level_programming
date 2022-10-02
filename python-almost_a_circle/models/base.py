ff33f1c7d6e70f4e3176a0d337abd11aad48a6a6
Salvador Borit <5084@holbertonschool.com>
2022-10-02 19:30:19 -0300

    test

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -2,6 +2,7 @@
 """Python interpreter"""
 
 
+from fileinput import filename
 import json
 from typing import Dict, List
 """JSON package"""
@@ -24,5 +25,16 @@ class Base:
     def to_json_string(list_dictionaries):
         """Serialize JSON to string"""
         if list_dictionaries is None:
-            return '[]'
+            return "[]"
+
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Serialize JSON to file"""
+        filename = f'{cls.__name__}.json'
+        json_list = []
+        for instance in list_objs:
+            json_list.append(cls.to_json_string(instance.to_dictionary()))
+        with open(filename, 'w') as file:
+                file.write(str(json_list))
