c5e8a22a3819c1f4c2884e8ae6b4e4dc7ccfd14b
Salvador Borit <5084@holbertonschool.com>
2022-10-03 15:16:40 -0300

    first test

--- python-almost_a_circle/tests/tests.py
+++ python-almost_a_circle/tests/tests.py
@@ -3,8 +3,14 @@
 
 
 import unittest
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestSquare(unittest.TestCase):
 
+    # Test of Base() for assigning automatically an ID exists
     def test_base_id(self):
+        b = Base()
+        self.assertTrue(b.id)
