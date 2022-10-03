13c0443dea4ed5a4314df0498c83c8d702fcd6db
Salvador Borit <5084@holbertonschool.com>
2022-10-03 15:23:26 -0300

    first test

--- python-almost_a_circle/tests/tests.py
+++ python-almost_a_circle/tests/tests.py
@@ -12,5 +12,9 @@ class TestSquare(unittest.TestCase):
 
     # Test of Base() for assigning automatically an ID exists
     def test_base_id(self):
-        b = Base()
-        self.assertTrue(b.id)
+        b1 = Base()
+        b2 = Base()
+        b3 = Base()
+        self.assertEqual(b1.id, 1)
+        self.assertEqual(b2.id, 2)
+        self.assertEqual(b3.id, 3)
