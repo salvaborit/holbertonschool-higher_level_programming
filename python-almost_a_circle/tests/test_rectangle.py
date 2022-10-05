50afbfe69c703992e72b82be5aea31d32e7f1602
Salvador Borit <5084@holbertonschool.com>
2022-10-05 15:43:43 -0300

    test

--- python-almost_a_circle/tests/test_rectangle.py
+++ python-almost_a_circle/tests/test_rectangle.py
@@ -9,20 +9,20 @@ from models.rectangle import Rectangle
 class TestRectangleInstantiation(unittest.TestCase):
     """Tests Rectangle instantiation"""
 
-    def test_1a(self):
+    def test_regular_instantiation_1(self):
         """Regular instantiation with args(1, 2)"""
         self.r = Rectangle(1, 2)
         self.assertEqual(self.r.width, 1)
         self.assertEqual(self.r.height, 2)
 
-    def test_1b(self):
+    def test_regular_instantiation_2(self):
         """Regular instantiation with args(1, 2, 3)"""
         self.r = Rectangle(1, 2, 3)
         self.assertEqual(self.r.width, 1)
         self.assertEqual(self.r.height, 2)
         self.assertEqual(self.r.x, 3)
 
-    def test_1c(self):
+    def test_regular_instantiation_3(self):
         """Regular instantiation with args(1, 2, 3, 4)"""
         self.r = Rectangle(1, 2, 3, 4)
         self.assertEqual(self.r.width, 1)
@@ -30,7 +30,7 @@ class TestRectangleInstantiation(unittest.TestCase):
         self.assertEqual(self.r.x, 3)
         self.assertEqual(self.r.y, 4)
 
-    def test_1d(self):
+    def test_regular_instantiation_4(self):
         """Regular instantiation with args(1, 2, 3, 4, 5)"""
         self.r = Rectangle(1, 2, 3, 4, 5)
         self.assertEqual(self.r.width, 1)
@@ -39,14 +39,14 @@ class TestRectangleInstantiation(unittest.TestCase):
         self.assertEqual(self.r.y, 4)
         self.assertEqual(self.r.id, 5)
 
-    def test_2a(self):
-        """Invalid parameter type on instantiation"""
+    def test_invalid_arg_type(self):
+        """Invalid argument type on instantiation"""
         self.assertRaises(TypeError, Rectangle, "1", 2)
         self.assertRaises(TypeError, Rectangle, 1, "2")
         self.assertRaises(TypeError, Rectangle, 1, 2, "3")
         self.assertRaises(TypeError, Rectangle, 1, 2, 3, "4")
 
-    def test_3(self):
+    def test_instantiation_with_negatives_zero(self):
         """Instantiation with negatives/zero"""
         self.assertRaises(ValueError, Rectangle, -1, 2)
         self.assertRaises(ValueError, Rectangle, 1, -2)
@@ -54,3 +54,18 @@ class TestRectangleInstantiation(unittest.TestCase):
         self.assertRaises(ValueError, Rectangle, 1, 0)
         self.assertRaises(ValueError, Rectangle, 1, 2, -3)
         self.assertRaises(ValueError, Rectangle, 1, 2, 3, -4)
+
+    def test_area(self):
+        """Area method"""
+        self.r = Rectangle(1, 2)
+        self.assertEqual(self.r.area(), 2)
+
+    def test__str__(self):
+        """___str___ method"""
+        self.r = Rectangle(1, 2, 3, 4, 5)
+        self.assertEqual(self.r.__str__(), '[Rectangle] (5) 3/4 - 1/2')
+
+    def test_display(self):
+        """Display method"""
+        self.r = Rectangle(2, 1, 0, 0)
+        self.assertEqual(self.r.display(), '##\n')
