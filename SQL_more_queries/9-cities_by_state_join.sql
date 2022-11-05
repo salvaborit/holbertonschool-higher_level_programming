2a430baa36e0598ef597cbd71c283029c618ff23
Salvador Borit <salvaborit@gmail.com>
2022-11-05 05:12:28 -0300

    task9

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,6 +1,4 @@
---lists all cities containes in db
+-- lists all cities containes in db
 
-SELECT cities.id, cities.name, states.name
-FROM cities
-JOIN states ON cities.state_id = states.id
-ORDER BY cities.id;
+SELECT cities.id, cities.name, states.name FROM cities
+JOIN states ON cities.state_id = states.id;
