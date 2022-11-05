943c0cf4093ce5a84086e706a6ccf3e7207b6bb6
Salvador Borit <salvaborit@gmail.com>
2022-11-05 01:49:32 -0300

    task0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,4 +1,4 @@
 -- lists privileges of users
 
-SHOW GRANTS FOR 'user_0d_2'@'%';
-SHOW GRANTS FOR 'user_0d_1'@'%';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
