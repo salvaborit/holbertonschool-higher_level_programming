1f78b9198a60f7c96fd69cf16b5bc9c43b9cdd41
Salvador Borit <salvaborit@gmail.com>
2022-11-05 19:31:51 -0300

    task17

---
+++ SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,11 @@
+-- converts (db, table, field) to utf8
+
+USE hbtn_0c_0;
+
+ALTER DATABASE hbtn_0c_0
+CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
+
+ALTER TABLE first_table
+CONVERT TO CHARACTER SET utf8mb4
+COLLATE utf8mb4_unicode_ci;
