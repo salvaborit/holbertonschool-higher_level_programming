69db77a55674d6839cca31bd9c8332418e0454b4
Salvador Borit <salvaborit@gmail.com>
2022-11-05 03:57:23 -0300

    task7

---
+++ SQL_more_queries/7-cities.sql
@@ -0,0 +1,12 @@
+-- creates database and table
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS cities (
+  id INT UNIQUE AUTO_INCREMENT NOT NULL,
+  state_id INT NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (state_id) REFERENCES states(id)
+);
