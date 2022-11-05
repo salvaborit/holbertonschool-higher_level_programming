9ab2bbbeac4e1b0683f6354553c04dd0e86f1fc9
Salvador Borit <salvaborit@gmail.com>
2022-11-05 08:00:40 -0300

    task

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- lists shows, genres linked to show
 -- from db 'hbtn_0d_tvshows'
 
-SELECT DISTINCT s.title, g.name
-FROM tv_shows s, tv_genres g, tv_show_genres sg
-WHERE (sg.show_id = s.id AND sg.genre_id = g.id)
+SELECT s.title, g.name FROM tv_shows s
+LEFT JOIN tv_show_genres sg ON s.id = sg.show_id
+LEFT JOIN tv_genres g ON g.id = sg.genre_id
 ORDER BY s.title, g.name;
