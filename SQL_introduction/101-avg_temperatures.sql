445a90e47a703bb80b8b707f7cc3b0914f951501
Salvador Borit <salvaborit@gmail.com>
2022-11-05 19:08:43 -0300

    task18

--- SQL_introduction/101-avg_temperatures.sql
+++ SQL_introduction/101-avg_temperatures.sql
@@ -1,6 +1,6 @@
 -- displays average temperature (F) by city
 
-SELECT city, AVG(value) AS average
+SELECT city, AVG(value) AS avg_temp
 FROM temperatures
 GROUP BY city
-ORDER BY average DESC;
+ORDER BY avg_temp DESC;
