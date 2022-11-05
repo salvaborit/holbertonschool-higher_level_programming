9f4c689a7c1adbeb1fb443e5ba474e0e68e1b697
Salvador Borit <salvaborit@gmail.com>
2022-11-05 16:19:00 -0300

    task14

--- SQL_more_queries/14-my_genres.sql
+++ SQL_more_queries/14-my_genres.sql
@@ -1,7 +1,13 @@
 -- lists all genres of the tv show 'Dexter' on db 'hbtn_0d_tvshows'
 
-SELECT g.name
-FROM tv_genres g
-RIght JOIN tv_show_genres sg
-ON g.id = sg.genre_id
-WHERE sg.show_id = 8;
+-- SELECT g.name
+-- FROM tv_genres g
+-- RIght JOIN tv_show_genres sg
+-- ON g.id = sg.genre_id
+-- WHERE sg.show_id = 8;
+
+
+SELECT g.name FROM tv_genres g
+JOIN tv_show_genres sg ON g.id = sg.genre_id
+JOIN tv_shows s ON s.id = sg.show_id
+WHERE s.id = 8;
