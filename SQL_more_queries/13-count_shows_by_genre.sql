d4857c0395009fc0a3e883f163c256455044b729
Salvador Borit <salvaborit@gmail.com>
2022-11-05 06:39:18 -0300

    task13

---
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -0,0 +1,9 @@
+-- lists all genres from db 'hbtn_0d_tvshows' with number of shows linked to each
+
+SELECT g.name AS genre,
+       COUNT(*) AS number_of_shows
+FROM tv_genres g
+JOIN tv_show_genres sg
+ON g.id = sg.genre_id
+GROUP BY g.name
+ORDER BY number_of_shows DESC;
