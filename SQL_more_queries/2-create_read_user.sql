141360b32c038cc9524543cce192c45badd22a61
Salvador Borit <salvaborit@gmail.com>
2022-11-05 03:22:12 -0300

    task2

---
+++ SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,8 @@
+-- creates database and user
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost'
+IDENTIFIED BY 'user_0d_2_pwd';
+
+GRANT SELECT on hbtn_0d_2.* to 'user_0d_2'@'localhost';
