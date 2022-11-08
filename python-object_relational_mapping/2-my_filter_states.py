e3d72b7db343d6134659ab2f126adc84ec202207
Salvador Borit <salvaborit@gmail.com>
2022-11-08 14:08:59 -0300

    2

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
-"""MODULE NAME"""
+"""task 2"""
 
 
+import MySQLdb
+import sys
 
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE '{}' ORDER BY id;".format(sys.argv[4]))
+    result = cursor.fetchall()
+    for record in result:
+        print(record)
+    cursor.close()
+    db.close()
