5858fbf9f0b496bad9d468e907065fbdadbafe46
Salvador Borit <salvaborit@gmail.com>
2022-11-08 13:42:40 -0300

    0

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
-"""MODULE NAME"""
+"""task 1"""
 
 
+import MySQLdb
+import sys
 
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states;")
+    result = cursor.fetchall()
+    for record in result:
+        print(record)
+    cursor.close()
+    db.close()
