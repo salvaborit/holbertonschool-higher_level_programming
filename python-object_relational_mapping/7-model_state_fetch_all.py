5f3a5f3887324ca95461f25f6e2e8d7d0ad85872
Salvador Borit <salvaborit@gmail.com>
2022-11-08 22:34:16 -0300

    7

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -2,9 +2,27 @@
 """MODULE NAME"""
 
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sys import argv
+from model_state import Base, State
 
-#!/usr/bin/python3
-"""MODULE NAME"""
+
+Base = declarative_base()
 
 
+if __name__ == '__main__':
 
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}')
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    result = session.query(State).all()
+    for i, item in enumerate(result):
+        print(f'{i + 1}: {item.name}')
+
+    session.close()
