827673bdfbd34a9e568b1445923415237186da58
Salvador Borit <salvaborit@gmail.com>
2022-11-08 23:29:43 -0300

    11

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -2,4 +2,27 @@
 """MODULE NAME"""
 
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sys import argv
+from model_state import Base, State
 
+
+Base = declarative_base()
+
+
+if __name__ == '__main__':
+
+    engine = create_engine(
+        f'mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}')
+    Base.metadata.create_all(bind=engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State()
+    new_state.name = 'Louisiana'
+    session.add(new_state)
+    session.commit()
+
+    session.close()
