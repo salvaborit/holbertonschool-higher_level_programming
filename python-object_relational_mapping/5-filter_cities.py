0fae192e8c0c2ff9183f0d9171be8b689e27ff3c
Salvador Borit <salvaborit@gmail.com>
2022-11-08 21:04:32 -0300

    5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
-"""MODULE NAME"""
+"""task 2"""
 
 
+import MySQLdb
+import sys
 
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+    )
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT c.name FROM cities c \
+        LEFT JOIN states s ON c.state_id = s.id \
+        WHERE s.name = %s \
+        ORDER BY c.id", (sys.argv[4], ))
+    result = cursor.fetchall()
+    for record in result:
+        print(record)
+    cursor.close()
+    db.close()
