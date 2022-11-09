e2e6df8f052d71a54a01ffd6f5e26f8df4dd105e
Salvador Borit <salvaborit@gmail.com>
2022-11-08 21:37:16 -0300

    6

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
-"""MODULE NAME"""
+"""State model"""
 
 
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 
+
+Base = declarative_base()
+
+
+class State(Base):
+    """class: State"""
+
+    __tablename__ = 'states'
+    id = Column('id', Integer, primary_key=True,
+                autoincrement=True, unique=True, nullable=False)
+    name = Column('name', String(128), nullable=False)
