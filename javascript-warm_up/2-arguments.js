59eb8af960d2b975436924fda580d081d717f947
Salvador Borit <salvaborit@gmail.com>
2022-12-10 16:02:32 -0300

    task2

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+if (process.argv.splice(2).length === 0) {
+  console.log('No argument');
+} else if (process.argv.splice(2).length === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
