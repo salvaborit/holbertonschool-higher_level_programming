ac9f685ad70661213382bb9cec18fd31d69351a8
Salvador Borit <salvaborit@gmail.com>
2022-12-10 22:30:17 -0300

    task11

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -4,10 +4,14 @@ const args = process.argv.splice(2);
 
 if (args.length === 0) {
     console.log(0);
-    break;
+    return;
 };
 
 args.forEach((item, index) => {
+  if (item[0] == '-') {
+    console.log(0);
+    process.exit();
+  }
   args[index] = parseInt(item);
 });
 args.sort((item, index) => item - index);
