e767296f5ac2b0b783984c254fe9f096fa55a2ef
Salvador Borit <salvaborit@gmail.com>
2022-12-10 21:49:24 -0300

    task10

--- javascript-warm_up/10-factorial.js
+++ javascript-warm_up/10-factorial.js
@@ -6,6 +6,6 @@ function factorial (n) {
 }
 
 if (isNaN(process.argv[2])) console.log(1);
-  else {
-    console.log(factorial(parseInt(process.argv[2])));
-  }
+else {
+  console.log(factorial(parseInt(process.argv[2])));
+}
