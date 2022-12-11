dac0ae31003fbd3add0fb4e6397c7bc756e10b62
Salvador Borit <salvaborit@gmail.com>
2022-12-11 20:16:34 -0300

    task3

---
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+module.exports =
+  class Rectangle {
+
+    width;
+    height;
+
+    constructor (w, h) {
+      if (parseInt(w) > 0 && parseInt(h) > 0) {
+        this.width = w;
+        this.height = h;
+      }
+    }
+
+    print () {
+      for (let row = 0; row < this.height; row++) {
+        for (let col = 0; col < this.width; col++) {
+          process.stdout.write('X')
+        }
+        console.log('')
+      }
+    }
+
+  };
