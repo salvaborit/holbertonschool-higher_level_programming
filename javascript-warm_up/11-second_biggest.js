1a6673c5e416b000097e7a3acc28689c14ca7ef1
Salvador Borit <salvaborit@gmail.com>
2022-12-10 23:00:38 -0300

    task100

--- javascript-warm_up/11-second_biggest.js
+++ javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,15 @@
 #!/usr/bin/node
-const myObject = {
-  type: 'object',
-  value: 12
-};
-console.log(myObject);
-myObject.value = 89;
-console.log(myObject);
+
+const args = process.argv.splice(2);
+
+if (args.length < 2) {
+  console.log(0);
+  process.exit();
+}
+
+args.forEach((item, index) => {
+  args[index] = parseInt(item);
+});
+args.sort((item, index) => item - index);
+
+console.log(args[args.length - 2]);
