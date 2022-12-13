22b7c0275b49774003341ef4d12566b3827ed1cd
Salvador Borit <salvaborit@gmail.com>
2022-12-12 23:03:23 -0300

    task5

---
+++ javascript-web_scraping/5-request_store.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const request = require('request');
+
+request.get({url: process.argv[2]}, (err, resp, body) => {
+  if (err) return;
+  fs.writeFile(process.argv[3], body, (err) => {
+    if (err) return;
+  });
+});
