4d457d4ceee431a8aa9bf0c6241d83e9e39e6927
Salvador Borit <salvaborit@gmail.com>
2022-12-12 21:26:45 -0300

    task4

---
+++ javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const options = {
+  url: process.argv[2],
+  method: 'GET'
+};
+
+request(options, function (err, resp, body) {
+  if (err) return;
+
+  const movies = JSON.parse(body).results;
+
+  let count = 0;
+  for (const movie of movies) {
+    const charList = movie.characters;
+    if (charList.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+      count++;
+    }
+  }
+  console.log(count);
+});
