ffa406e99dc0571b04aed4e04d4df7733976e25c
Salvador Borit <salvaborit@gmail.com>
2022-12-18 01:35:33 -0300

    task5

--- javascript-dom_manipulation/5-script.js
+++ javascript-dom_manipulation/5-script.js
@@ -1,3 +1,6 @@
-#!/usr/bin/node
+const update_header = document.querySelector('#update_header');
+const header = document.querySelector('header');
 
-
+update_header.addEventListener('click', () => {
+  header.innerText = 'New header!!!';
+});
